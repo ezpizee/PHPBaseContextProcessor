d682cc9e8fb99adfbb8365762840db299e90ffaf
Sothea Nim <sothea.nim@ezpizee.com>
2021-01-01 17:49:52 -0500

    initial commit

---
+++ src/Ezpizee/ContextProcessor/Base.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Ezpizee\ContextProcessor;
+
+use Ezpizee\MicroservicesClient\Client;
+use Ezpizee\Utils\Request;
+use Ezpizee\Utils\RequestEndpointValidator;
+
+abstract class Base
+{
+  protected $context = [
+    'message' => 'success',
+    'code' => 200,
+    'data' => null,
+    'debug' => null
+  ];
+
+  /**
+   * @var Request
+   */
+  protected $request;
+  /**
+   * @var Client
+   */
+  protected $microserviceClient;
+
+  protected $requestData = [];
+
+  public function __construct(Client $client = null) {if (!empty($client)) {$this->microserviceClient = $client;}}
+
+  public function setMicroServiceClient(Client $client): void {$this->microserviceClient = $client;}
+
+  public function setRequest(Request $request) {
+    $this->request = $request;
+    $this->requestData = $request->getRequestParamsAsArray();
+  }
+
+  public function setRequestData(array $data): void {$this->requestData = $data;}
+
+  abstract public function methods(): array;
+
+  abstract public function exec(): void;
+
+  abstract public function validRequiredParams(): bool;
+
+  protected final function getUriParam($key): string {return RequestEndpointValidator::getUriParam($key);}
+
+  public final function setContextData(array $data): void{$this->context['data'] = $data;}
+
+  public final function setContextCode(int $code): void {$this->context['code'] = $code;}
+
+  public final function setContextMessage(string $msg): void {$this->context['message'] = $msg;}
+
+  public final function setContextDebug(array $debug): void {$this->context['debug']=$debug;}
+
+  public final function getContext(): array {return $this->context;}
+
+  public final function getContextCode(): int {return is_string($this->context['code'])?(int)$this->context['code']:$this->context['code'];}
+
+  public final function getContextMessage(): string {return $this->context['message'];}
+
+  public final function getContextData(): array {return empty($this->context['data']) ? [] : $this->context['data'];}
+
+  public final function getContextDebug(): array {return empty($this->context['debug']) ? [] : $this->context['debug'];}
+}
