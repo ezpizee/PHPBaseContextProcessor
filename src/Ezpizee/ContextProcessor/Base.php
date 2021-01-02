5ad09e71ce1746fa92240d73b94840de4caae2d1
Sothea Nim <sothea.nim@ezpizee.com>
2021-01-02 10:43:57 -0500

    improved

--- src/Ezpizee/ContextProcessor/Base.php
+++ src/Ezpizee/ContextProcessor/Base.php
@@ -79,7 +79,11 @@ abstract class Base
      */
     protected final function defaultRequiredParamsValidator(string $configFilePath=''): bool
     {
-        if (!$configFilePath && defined('ROOT_DIR')) {
+        if (!$configFilePath) {
+            if (!defined('ROOT_DIR')) {
+                $dir = explode('/vendor/', __DIR__);
+                define('ROOT_DIR', $dir[0]);
+            }
             $configFilePath = ROOT_DIR . DS . str_replace(
                     ['ContextProcessor', '\\'],
                     ['', DS],
